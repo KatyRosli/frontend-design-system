cfb2b4f1442f14e15c89f27c2c299f9e4df4751f
Katy Rosli <katyrosli@gmail.com>
2026-07-09 11:20:44 +0200

    setup storybook with tailwind

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,36 @@
-import type { StorybookConfig } from '@storybook/nextjs-vite';
+import type { StorybookConfig } from "@storybook/nextjs-vite";
+import path from "path";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    "../**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  "addons": [
+
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-vitest",
     "@storybook/addon-a11y",
     "@storybook/addon-docs",
-    "@storybook/addon-mcp"
+    "@storybook/addon-mcp",
+    "@storybook/addon-styling-webpack",
   ],
-  "framework": "@storybook/nextjs-vite",
-  "staticDirs": [
-    "../public"
-  ]
+
+  framework: "@storybook/nextjs-vite",
+
+  staticDirs: ["../public"],
+
+  viteFinal: async (config) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve?.alias,
+        "@": path.resolve(__dirname, "../"),
+      },
+    };
+
+    return config;
+  },
 };
+
 export default config;
